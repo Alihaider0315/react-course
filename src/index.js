4d47c54b38be9af7a025bcb16753d99c5c0a7435
Alihaider0315 <ah712200@gmail.com>
2023-08-15 15:28:09 +0500

    code updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BookList from './Booklist';
+import Book from './Book';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BookList/>
+    <Book/>
   </React.StrictMode>
 );
